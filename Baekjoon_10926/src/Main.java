3973af866228df7ac0f8f827a05b0912ee4ea7b3
whdgus1323 <whdgus1323@gmail.com>
2023-10-25 13:37:03 +0900

    no message

--- Baekjoon_10926/src/Main.java
+++ Baekjoon_10926/src/Main.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -11,13 +10,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
+		String id = br.readLine();
+		id += "??!";
 		
-		
-		bw.write(String.valueOf(x * y));
+		bw.write(id);
 		
 		br.close();
 		bw.flush();
