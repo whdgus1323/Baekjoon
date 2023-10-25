9810a5d97a273b291d8b76a96f71a3e9d326d2be
whdgus1323 <whdgus1323@gmail.com>
2023-10-25 12:47:35 +0900

    commit

---
+++ Baekjoon_2557/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("Hello World!\n");
+		bw.close();
+	}
+
+}
