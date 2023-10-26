774f7acbb3c15c6a7ade158a0b83ff77f17b25a7
whdgus1323 <whdgus1323@gmail.com>
2023-10-26 10:57:53 +0900

    no message

---
+++ 조건문_Baekjoon_14681/src/조건문_Baekjoon_2480/Main.java
@@ -0,0 +1,31 @@
+package 조건문_Baekjoon_2480;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		
+		if(x > 0 && y > 0) bw.write(String.valueOf(1));
+		else if(x < 0 && y > 0) bw.write(String.valueOf(2));
+		else if (x > 0 && y < 0) bw.write(String.valueOf(4));
+		else bw.write(String.valueOf(3));
+		
+		br.close();
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
